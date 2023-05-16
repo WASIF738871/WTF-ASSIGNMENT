19b5315a90a7b82edb9753a7dbf1b1d2d70fef3b
WASIF738871 <erwasifmohd786@gmail.com>
2023-05-16 08:04:48 +0530

    revert practice

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 import React from 'react';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import NaveBarComponent from './components/navebar/naveBarComponent';
-import Login from './pages/loginPage/login';
-import Home from './pages/homePage/home';
-import Fitness from './pages/fitnessPage/fitness';
-import Nutrition from './pages/nutritionPage/nutrition';
-import Zyms from './pages/zyms/zyms';
-import Partners from './pages/partners/partner';
-import About from './pages/about/about';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-
-lkjakljhfjdsakhjdfahjkhagkhgghfdjjhajh
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NaveBarComponent/>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="fitness" element={<Fitness/>} />
-      <Route path="nutrition" element={<Nutrition/>} />
-      <Route path="zyms" element={<Zyms/>} />
-      <Route path="partners" element={<Partners/>} />
-      <Route path="about" element={<About/>} />
-      <Route path="login" element={<Login/>} />
-    </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
